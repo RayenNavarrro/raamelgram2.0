9bdbe5dc397660a83c8e118e80c88aa883bc0351
RayenNavarrro <petgan04@gmail.com>
2021-12-21 15:32:54 +0100

    port

--- models/images.js
+++ models/images.js
@@ -1,7 +1,4 @@
-const { Int32 } = require('bson');
 var mongoose = require('mongoose');
-const internal = require('stream');
-const { createInflate } = require('zlib');
 
 var imageSchema = new mongoose.Schema({
 	name: String,
